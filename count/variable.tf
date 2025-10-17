3e1d6e1c9ea4a2bb284045a906357a503230e95d
gopikrishna <gopikrishnamaneem@gmail.com>
2025-10-17 10:57:15 +0530

    terraform

---
+++ count/variable.tf
@@ -0,0 +1,9 @@
+variable "instances" {
+    default = ["mongodb","redis","rabbitmq","mysql"]
+}
+variable "zone_id" {
+    default = "Z0379910K30ZWY5SALMW"
+}
+variable "domain_name" {
+    default = "gopi29.fun"
+}
---
+++ variables/variables.tf
@@ -0,0 +1,46 @@
+variable "ami_id" {
+    type = string
+  default     = "ami-09c813fb71547fc4f"
+}
+
+variable "instance_type" {
+    type = string
+  default     = "t3.micro"
+}
+
+variable "ec2_tags" {
+    type = map
+    default = {
+        Name = "terraform-demo"
+        Terraform = "true"
+        project = "joindevops"
+        Environment = "dev"
+    }
+}
+
+variable "sg_name" {
+    type = string
+    default = "allow"
+    description = "security group name"
+}
+
+variable "cidr" {
+    default = ["0.0.0.0/0"]
+}
+
+variable "ingress_from_port" {
+    default = 0
+}
+variable "ingress_to_port" {
+    default = 0
+}
+variable "egress_from_port" {
+    default = 0
+}
+variable "egress_to_port" {
+    default = 0
+}
+variable "protocol"{
+    type = string
+    default = "-1"
+}
