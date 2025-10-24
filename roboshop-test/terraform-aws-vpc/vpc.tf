04c9f12ceeb7caf973ff02063fa6404277c441d8
gopikrishna <gopikrishnamaneem@gmail.com>
2025-10-24 08:40:37 +0530

    vpc

---
+++ roboshop-test/terraform-aws-vpc/vpc.tf
@@ -0,0 +1,44 @@
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block       = var.vpc_cidr
+  instance_tenancy = "default"
+  enable_dns_hostnames = true
+
+  tags = merge(
+    var.vpc_tags,
+    local.common_tags,
+    {
+        Name = local.common_name_suffix
+    }
+  )
+}
+
+# IGW
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.igw_tags,
+    local.common_tags,
+    {
+        Name = local.common_name_suffix
+    }
+  )
+}
+
+# Public Subnets
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    var.public_subnet_tags,
+    local.common_tags,
+    {
+        Name = "${local.common_name_suffix}-public-${local.az_names[count.index]}" # roboshop-dev-public-us-east-1a
+    }
+  )
+}
